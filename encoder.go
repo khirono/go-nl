8ada85ed176ebebfeb82949a595be99df005873c
HIRONO Koji <koji.hirono.yh@apresiasystems.co.jp>
2021-09-21 11:49:46 +0900

    Add Encoders

--- encoder.go
+++ encoder.go
@@ -4,3 +4,25 @@ type Encoder interface {
 	Len() int
 	Encode([]byte) (int, error)
 }
+
+type Encoders []Encoder
+
+func (es Encoders) Len() int {
+	n := 0
+	for _, e := range es {
+		n += e.Len()
+	}
+	return n
+}
+
+func (es Encoders) Encode(b []byte) (int, error) {
+	off := 0
+	for _, e := range es {
+		n, err := e.Encode(b[off:])
+		if err != nil {
+			return off, err
+		}
+		off += n
+	}
+	return off, nil
+}
